e0325ec205638736104db32d9ba3851ed083ac33
Tatiane Entler <94848789+tatientler@users.noreply.github.com>
2022-01-18 18:02:50 -0300

    Novas classes

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import {Cliente} from "./Cliente.js"
+import { Gerente } from "./Funcionários/Gerente.js"
+import { Diretor } from "./Funcionários/Diretor.js"
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js"
+
+/* Informações de utilização das contas
+
 import {ContaCorrente} from "./Conta/ContaCorrente.js"
 import { ContaPoupanca } from "./Conta/ContaPoupanca.js"
 import { ContaSalario } from "./Conta/ContaSalario.js"
@@ -11,7 +17,6 @@ const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001)
 contaCorrenteRicardo.depositar(500)
 const valorSacado = contaCorrenteRicardo.sacar(50)
 
-/*
 contaCorrenteRicardo.depositar(100)
 contaCorrenteRicardo.depositar(100)
 contaCorrenteRicardo.depositar(100)
@@ -28,7 +33,6 @@ console.log(conta2)
 console.log(conta2.saldo)
 console.log(contaCorrenteRicardo.cliente)
 console.log(ContaCorrente.numeroDeContas)
-*/
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
 contaPoupanca.sacar(10)
@@ -36,12 +40,27 @@ contaPoupanca.sacar(10)
 console.log(contaPoupanca)
 console.log(contaCorrenteRicardo)
 
-/* Teste de erro criado
+*** Teste de erro criado
 const conta = new Conta(0, cliente1, 1001) 
 console.log(conta)  
-*/
+***
 
 const contaSalario = new ContaSalario(cliente1)
 contaSalario.depositar(200)
 contaSalario.sacar(20)
 console.log(contaSalario)
+*/
+
+const diretor = new Diretor ("Rodrigo", 33344455566, 10000)
+diretor.cadastrarSenha("123456789")
+
+const gerente = new Gerente ("Ricardo", 66677788899, 5000)
+gerente.cadastrarSenha("123456")
+
+const cliente = new Cliente("Lais", 77788899900, "456")
+
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456789")
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123456")
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456")
+
+console.log(clienteEstaLogado)
